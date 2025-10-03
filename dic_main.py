a5cda2662f5204dcd4067fab0bca36934534a4df
AlexSacMorane <al.sacmorane@gmail.com>
2025-10-03 21:04:15 +0200

    apply the dic functions

--- dic_main.py
+++ dic_main.py
@@ -14,6 +14,92 @@ from dic_parameters import parameters
 # Functions
 #-------------------------------------------------------------------------------
 
+def dic(dict_user):
+    '''
+    Apply the DIC algorithm.
+    '''
+    # initialization 
+    l_sample = dict_user['l_zs_min']
+    L_u = []
+    L_lc_sample = []
+    # iterate on lines for sample
+    while l_sample < dict_user['l_zs_max']:
+        # user
+        print(l_sample, '/', dict_user['l_zs_max'])
+
+        # initialization of the sample
+        c_sample = dict_user['c_zs_min']
+        # iterate on column for sample
+        while c_sample < dict_user['c_zs_max']:
+            # define sample
+            sample = dict_user['M_final'][-1-l_sample-dict_user['d_sample']:-1-l_sample, c_sample:c_sample+dict_user['d_sample']]
+            # define search zone
+            search_zone = dict_user['M_initial'][-1-l_sample-dict_user['d_sample']-dict_user['d_zr']:-1-l_sample+dict_user['d_zr'], c_sample-dict_user['d_zr']:c_sample+dict_user['d_sample']+dict_user['d_zr']]
+            # look sample in the search zone
+            u = look_sample_in_search_zone(sample, search_zone)
+            # adapt u
+            u[0] = u[0] - dict_user['d_zr']
+            u[1] = u[1] - dict_user['d_zr']
+            # save 
+            L_u.append(u)
+            L_lc_sample.append([l_sample, c_sample])
+            # next column
+            c_sample = c_sample + dict_user['d_sample']
+        # next line
+        l_sample = l_sample + dict_user['d_sample']
+    # save
+    dict_user['L_u'] = L_u
+    dict_user['L_lc_sample'] = L_lc_sample
+
+#-------------------------------------------------------------------------------
+
+def look_sample_in_search_zone(sample, search_zone):
+    '''
+    Search sample in the search zone.
+    '''
+    # initialization
+    u_l = 0
+    u_c = 0
+    cor_max = 0
+    # iterate on lines and column in search_zone
+    for l in range(search_zone.shape[0]-sample.shape[0]):
+        for c in range(search_zone.shape[1]-sample.shape[1]):
+            cor = normxcorr2(sample, search_zone[-1-l-sample.shape[0]: -1-l, c: c+sample.shape[1]])
+            # look for the maximum value
+            if cor > cor_max:
+                cor_max = cor
+                # change
+                u_l = l
+                u_c = c
+    return [u_l, u_c]
+
+#-------------------------------------------------------------------------------
+
+def normxcorr2(M_sample, M_sz):
+    '''
+    Normalized 2-D cross-correlation.
+    '''
+    # initialization of sums
+    S_xy = 0
+    S_x2 = 0
+    S_y2 = 0
+    # iterate on lines and columns
+    for l in range(M_sample.shape[0]):
+        for c in range(M_sample.shape[1]):
+            S_xy = (M_sample[l,c] - np.mean(M_sample))*(M_sz[l,c] - np.mean(M_sz))
+            S_x2 = (M_sample[l,c] - np.mean(M_sample))**2
+            S_y2 = (M_sz[l,c] - np.mean(M_sz))**2
+    # compute correlation
+    cor = S_xy/(math.sqrt(S_x2)*math.sqrt(S_y2))
+    return cor
+
+#-------------------------------------------------------------------------------
+
+def pp(dict_user):
+    '''
+    Post-process the dic output
+    '''
+    pass
 
 #------------------------------------------------------------------------------
 # load dict
